8c1c37272f54c698eef0a12e6398d59620d452ed
Pemba Sherpa <pembas@trilogycare.com.au>
2025-02-01 11:17:29 +1030

    feat: menu mmfs added

---
+++ app/Models/Menu.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    /** @use HasFactory<\Database\Factories\MenuFactory> */
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'category_id'
+    ];
+}
