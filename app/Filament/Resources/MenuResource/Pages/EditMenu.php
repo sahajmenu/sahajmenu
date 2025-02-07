1364a4eaba95ac77061d036eddefd360bcc91eee
pemba17 <pemba.nuru59@gmail.com>
2025-02-07 22:09:37 +1030

    refactor: duster lint fixed

--- app/Filament/Resources/MenuResource/Pages/EditMenu.php
+++ app/Filament/Resources/MenuResource/Pages/EditMenu.php
@@ -3,7 +3,7 @@
 namespace App\Filament\Resources\MenuResource\Pages;
 
 use App\Filament\Resources\MenuResource;
-use Filament\Actions;
+use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditMenu extends EditRecord
@@ -13,7 +13,7 @@ class EditMenu extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
-            Actions\DeleteAction::make(),
+            DeleteAction::make(),
         ];
     }
 }
--- app/Filament/Resources/MenuResource/Pages/ListMenus.php
+++ app/Filament/Resources/MenuResource/Pages/ListMenus.php
@@ -3,7 +3,7 @@
 namespace App\Filament\Resources\MenuResource\Pages;
 
 use App\Filament\Resources\MenuResource;
-use Filament\Actions;
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
 class ListMenus extends ListRecords
@@ -13,7 +13,7 @@ class ListMenus extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            CreateAction::make(),
         ];
     }
 }
